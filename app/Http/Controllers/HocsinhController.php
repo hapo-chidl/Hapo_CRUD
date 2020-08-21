f2a39f008efdeb9548d7e1bab4aef87af4c0a97c
linhchi2406 <linhchi24060508@gmail.com>
2020-08-21 13:14:05 +0700

    Edit file

--- app/Http/Controllers/HocsinhController.php
+++ app/Http/Controllers/HocsinhController.php
@@ -87,7 +87,7 @@ class HocsinhController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $updateData = DB::table('studentư')->where('id', $request->id)->update([
+        $updateData = DB::table('students')->where('id', $request->id)->update([
             'full_name' => $request->full_name,
             'email' => $request->email,
             'avatar' => $request->avatar,
