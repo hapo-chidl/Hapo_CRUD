83cdb0addd8c6799185efb8efe277a067c8df544
linhchi2406 <linhchi24060508@gmail.com>
2020-08-21 14:04:19 +0700

    Edit file

--- resources/views/Templates/create.blade.php
+++ resources/views/Templates/create.blade.php
@@ -4,30 +4,24 @@
 <div class="page-header">
     <h4>Quản lý học sinh</h4>
 </div>
-	
-<?php ?>
 @if ( Session::has('success') )
-<div class="alert alert-success alert-dismissible" role="alert">
-    <strong>{{ Session::get('success') }}</strong> 
-	<button aria-label="Close" class="close" data-dismiss="alert" type="button">
-	<span aria-hidden="true">&times;</span> 
-	<span class="sr-only">Close</span>
-	</button>
-</div>
+    <div class="alert alert-success alert-dismissible" role="alert">
+        <strong>{{ Session::get('success') }}</strong> 
+	    <button aria-label="Close" class="close" data-dismiss="alert" type="button">
+	        <span aria-hidden="true">&times;</span> 
+	        <span class="sr-only">Close</span>
+	    </button>
+    </div>
 @endif 
-	
-<?php ?> 
 @if ( Session::has('error') )
-<div class="alert alert-danger alert-dismissible" role="alert">
-    <strong>{{ Session::get('error') }}</strong> 
-	<button aria-label="Close" class="close" data-dismiss="alert" type="button">
-		<span aria-hidden="true">&times;</span> 
-		<span class="sr-only">Close</span>
-	</button>
-</div>
+    <div class="alert alert-danger alert-dismissible" role="alert">
+        <strong>{{ Session::get('error') }}</strong> 
+	    <button aria-label="Close" class="close" data-dismiss="alert" type="button">
+		    <span aria-hidden="true">&times;</span> 
+		    <span class="sr-only">Close</span>
+	    </button>
+    </div>
 @endif 
-<?php ?>
-    
 <p><a class="btn btn-primary" href="{{ url('/student') }}">Về danh sách</a></p>
 <div class="col-xs-4 col-xs-offset-4">
     <h4>Thêm học sinh</h4>
@@ -36,34 +30,28 @@
         <div class="form-group">
             <label for="Fullname">Full name</label>
 			<input class="form-control" id="full_name" maxlength="255" name="full_name" placeholder="Fullname" required="" type="text">
-				
-			<?php ?> 
 			@if ($errors->has('Fullname'))
-            <div class="alert alert-danger">
-                {{ $errors->first('Fullname') }}
-            </div>
+                <div class="alert alert-danger">
+                    {{ $errors->first('Fullname') }}
+                </div>
 			@endif
         </div>
         <div class="form-group">
             <label for="email">Email</label> 
 			<input class="form-control" id="email" name="email" placeholder="Email" required="" type="text"> 
-				
-			<?php ?>
 			@if ($errors->has('email'))
-            <div class="alert alert-danger">
-                {{ $errors->first('email') }}
-            </div>
+                <div class="alert alert-danger">
+                    {{ $errors->first('email') }}
+                </div>
 			@endif
         </div>
         <div class="form-group">
             <label for="address">Address</label> 
 			<input class="form-control" id="address" name="address" placeholder="Address" required="" type="text"> 
-				
-			<?php ?>
 			@if ($errors->has('address'))
-            <div class="alert alert-danger">
-                {{ $errors->first('address') }}
-            </div>
+                <div class="alert alert-danger">
+                    {{ $errors->first('address') }}
+                </div>
 			@endif
         </div>
         <div class="form-group">
