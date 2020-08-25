e08ab8bcd6b31463cffca282e81e3d5ba7a492a4
linhchi2406 <linhchi24060508@gmail.com>
2020-08-25 14:30:54 +0700

    Add file

--- resources/views/student/master.blade.php
+++ resources/views/student/master.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title> @yield('title')</title>
     <link href="{{ asset('css/bootstrap.min.css') }}" type="text/css" rel="stylesheet" />
-    <link href="{ asset('css/dataTables.bootstrap.min.css') }}" type="text/css" rel="stylesheet" />
+    <link href="{{ asset('css/dataTables.bootstrap.min.css') }}" type="text/css" rel="stylesheet" />
     <link href="{{ asset('css/bootstrap-custom.css') }}" type="text/css" rel="stylesheet" />
 </head>
 <body>
@@ -13,9 +13,9 @@
         @section('content')
         @show
     </div>
-    <script type="text/javascript" src=" {{ asset('js/jquery.min.js') }}"></script>
-    <script type="text/javascript" src=" {{ asset('js/bootstrap.min.js') }}"></script>
-    <script type="text/javascript" src=" {{ asset('js/jquery.dataTables.min.js') }}"></script>
-    <script type="text/javascript" src=" {{ asset('js/dataTables.bootstrap.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/jquery.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/jquery.dataTables.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/dataTables.bootstrap.min.js') }}"></script>
 </body>
 </html>
