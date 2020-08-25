9f68bcf1504bc13e7a46094a447e582617659890
linhchi2406 <linhchi24060508@gmail.com>
2020-08-25 10:38:40 +0700

    Add file

--- resources/views/student/edit.blade.php
+++ resources/views/student/edit.blade.php
@@ -18,8 +18,7 @@
 @if ( Session::has('error') )
     <div class="alert alert-danger alert-dismissible" role="alert">
         <strong>{{ Session::get('error') }}</strong> 
-	    <button aria-label="Close" class="close" data-dismiss="alert"
-        type="button"><span aria-hidden="true">&times;</span> <span class="sr-only">Close</span></button>
+	    <button aria-label="Close" class="close" data-dismiss="alert" type="button"><span aria-hidden="true">&times;</span> <span class="sr-only">Close</span></button>
     </div>
 @endif 
 
